1583de844cc161c526affa122c024acda46fd0d2
brennan-wright <brennan@nmedia3.com>
2021-09-25 10:53:33 -0700

    IT WORKS

--- frontend/static/formset/jquery.formset.js
+++ frontend/static/formset/jquery.formset.js
@@ -227,10 +227,10 @@
     $.fn.formset.defaults = {
         prefix: 'form', // The form prefix for your django formset
         formTemplate: null, // The jQuery selection cloned to generate new form instances
-        addText: 'add another', // Text for the add link
-        deleteText: 'remove', // Text for the delete link
-        addCssClass: 'add-row', // CSS class applied to the add link
-        deleteCssClass: 'delete-row', // CSS class applied to the delete link
+        addText: 'Add +', // Text for the add link
+        deleteText: '-', // Text for the delete link
+        addCssClass: 'btn btn-primary', // CSS class applied to the add link
+        deleteCssClass: 'btn btn-danger', // CSS class applied to the delete link
         formCssClass: 'dynamic-form', // CSS class applied to each form in a formset
         extraClasses: [], // Additional CSS classes, which will be applied to each form in turn
         keepFieldValues: '', // jQuery selector for fields whose values should be kept when the form is cloned
