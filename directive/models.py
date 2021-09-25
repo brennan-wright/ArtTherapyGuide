fa233f50f80dcd9876db8166e6489c006f9afbb6
brennan-wright <brennan@nmedia3.com>
2021-09-25 11:06:51 -0700

    added directive as a numbered list.

--- directive/models.py
+++ directive/models.py
@@ -62,7 +62,7 @@ class DirectiveObjective(models.Model):
     objective = models.CharField(
         max_length=250, verbose_name="Directive Objectives", null=False, blank=False)
     directive = models.ForeignKey(
-        DirectivePage, on_delete=models.CASCADE, null=False, blank=False)
+        DirectivePage, on_delete=models.CASCADE, related_name='objectives', null=False, blank=False)
 
     def __str__(self):
         return self.objective
