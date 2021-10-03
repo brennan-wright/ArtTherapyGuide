50bfaef5bcd64e433bf597338a37764acf99d99a
brennan-wright <brennan@nmedia3.com>
2021-10-03 10:35:02 -0700

    add sitemaps!

--- arttherapy/urls.py
+++ arttherapy/urls.py
@@ -1,15 +1,37 @@
 
 import debug_toolbar
 from directive import urls as directive_urls
+from directive.models import DirectivePage
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 from django.views.generic.base import TemplateView
 from users import urls as users_urls
 
+from arttherapy.sitemaps import (AboutSitemap, DirectiveIndexSitemap,
+                                 DirectiveNewSitemap, HomeSitemap,
+                                 LoginSitemap, ProfileSitemap, SignupSitemap)
+
 from . import views as health_check
 from .views import AboutView, HomeView
+
+sitemaps = {
+    'Directives': GenericSitemap({
+        'queryset': DirectivePage.objects.all().order_by('id'),
+        'date_field': 'updated',
+    }),
+    'directive-index': DirectiveIndexSitemap,
+    'directive-new': DirectiveNewSitemap,
+    'login': LoginSitemap,
+    'signup': SignupSitemap,
+    'about': AboutSitemap,
+    'home': HomeSitemap,
+    'profile': ProfileSitemap,
+
+}
 
 urlpatterns = [
     path(
@@ -17,6 +39,9 @@ urlpatterns = [
         TemplateView.as_view(template_name="robots.txt",
                              content_type="text/plain"),
     ),
+    path('sitemap.xml', sitemap,
+         {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
     url(r'^health_check/', health_check.health_check.as_view()),
     url(r'^django-admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
