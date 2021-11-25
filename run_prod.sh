a8aabaf4bdd4b80ad221ae28307786b9488afba2
brennan-wright <brennan@nmedia3.com>
2021-11-25 11:57:19 -0800

    Modify gunicorn setup

--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput &&
 python manage.py migrate --noinput &&
-gunicorn arttherapy.wsgi --bind 0.0.0.0:8000
+gunicorn arttherapy.wsgi --worker-tmp-dir /dev/shm --workers=2 --threads=4 --bind 0.0.0.0:8000 
